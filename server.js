61e9272bbcb1f73f6ce58ad9ad43dc716deccbce
Apurva Priyadarshi <apriyadarshi.1995@gmail.com>
2017-04-03 20:08:17 +0530

    Adding Express framework

---
+++ server.js
@@ -0,0 +1,35 @@
+var express= require('express');
+var fs= require('fs');
+var app=express();
+var shell_escape=require('shell-escape');
+
+app.get('/online.html',function(req,res) {
+  res.sendFile( __dirname +"/"+ "online.html");
+})
+
+app.get('/form_get',function(req,res){
+ //var input = document.getElementById('fname');
+ //if(input.value.length == 0) input.value = "untitled.chpl";
+ fs.writeFile(req.query.filename,req.query.codeeditor,null,function(err,data){
+          if(err) throw err;
+          else{
+          	const exec = require('child_process').exec;
+                var arg1=['chpl' , req.query.filename];
+		exec(shell_escape(arg1), (err, stdout, stderr) => {
+  		if (err) {
+    			console.error(err);
+    			return;
+      			 }
+                var arg2=['./a.out'];
+                exec(shell_escape(arg2), (err, stdout, stderr) => { console.log(stdout);
+                
+                });
+                });   
+              }
+              
+ });  
+})
+
+app.listen(8000,function(){
+  console.log("Server UP");
+})
