ad874388435dcaea0442aa2ba6dcaad69568f8d6
Apurva Priyadarshi <apriyadarshi.1995@gmail.com>
2017-04-12 18:07:21 +0530

    The platform is now working .

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ var express= require('express');
 var fs= require('fs');
 var app=express();
 var shell_escape=require('shell-escape');
+const pug = require('pug');
 
-app.get('/online.html',function(req,res) {
+/*app.get('/online.html',function(req,res) {
   res.sendFile( __dirname +"/"+ "online.html");
+})
+*/
+app.set('view engine', 'pug')
+app.get('/', function (req, res) {
+  res.render('mainpage.pug')
 })
 
 app.get('/form_get',function(req,res){
@@ -13,15 +19,16 @@ app.get('/form_get',function(req,res){
  fs.writeFile(req.query.filename,req.query.codeeditor,null,function(err,data){
           if(err) throw err;
           else{
-          	const exec = require('child_process').exec;
+              	const exec = require('child_process').exec;
                 var arg1=['chpl' , req.query.filename];
-		exec(shell_escape(arg1), (err, stdout, stderr) => {
-  		if (err) {
-    			console.error(err);
-    			return;
-      			 }
+	         	     exec(shell_escape(arg1), (err, stdout, stderr) => {
+  		            if (err) {
+    			               console.error(err);
+    			                 return;
+      			               }
                 var arg2=['./a.out'];
-                exec(shell_escape(arg2), (err, stdout, stderr) => { console.log(stdout);
+                exec(shell_escape(arg2), (err, stdout, stderr) => { 
+                res.render('mainpage', { myOutput: stdout }) 
                 
                 });
                 });   
@@ -33,3 +40,4 @@ app.get('/form_get',function(req,res){
 app.listen(8000,function(){
   console.log("Server UP");
 })
+
